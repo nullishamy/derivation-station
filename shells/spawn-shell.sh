b1a273c0969130a18a9bf8eea8b7685bbdddc955
nullishamy <amy.codes@null.net>
2022-11-19 22:22:57 +0000

    fix: spawn-shell

--- shells/spawn-shell.sh
+++ shells/spawn-shell.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ "$#" -gt 1 ]]; then
-    # Run the shell given the rest of the input as extra packages
     extras="${@:2}"
-    nix-shell -E "with import <nixpkgs> { }; runCommand \"shell\" { buildInputs = [ $extras ]; } \"\""
+    nix-shell ~/shells/"$1".nix --arg extras "with import <nixpkgs> {}; [ $extras ]"
 else
     nix-shell ~/shells/"$1".nix
 fi
