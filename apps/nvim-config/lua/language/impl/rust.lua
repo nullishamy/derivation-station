ad4fa97172c1ef7229419bb07de6b2a5691bb788
nullishamy <amy.codes@null.net>
2022-12-10 01:28:30 +0000

    fix: no inlay hints

--- apps/nvim-config/lua/language/impl/rust.lua
+++ apps/nvim-config/lua/language/impl/rust.lua
@@ -14,13 +14,8 @@ return {
               capabilities = config.capabilities,
             },
             tools = {
-              autoSetHints = false,
               inlay_hints = {
-                show_parameter_hints = false,
-                show_variable_name = false,
-                on_initialized = function()
-                  require('rust-tools').inlay_hints.disable()
-                end,
+                auto = false,
               },
             },
           })
