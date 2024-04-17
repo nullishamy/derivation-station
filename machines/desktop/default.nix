2fc732c47ee9fe58c5a3d160d4dc063e8ff49b1b
nullishamy <git@amyerskine.me>
2024-04-17 18:10:56 +0100

    fix: misc

--- machines/desktop/default.nix
+++ machines/desktop/default.nix
@@ -112,6 +112,8 @@ in {
   virtualisation.docker = {
     enable = true;
   };
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = ["amy"];
 
   # Fonts
   fonts.packages = with pkgs; [
