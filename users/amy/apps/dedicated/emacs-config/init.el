8f1860458aa7500a88b444d36549f2e2f8c7aab2
nullishamy <git@amyerskine.me>
2024-06-03 23:24:53 +0100

    fix: org; style

--- users/amy/apps/dedicated/emacs-config/init.el
+++ users/amy/apps/dedicated/emacs-config/init.el
@@ -1,3 +1,26 @@
+;; Configure straight.el settings
+(setq straight-enable-use-package-integration t)
+(setq straight-use-package-by-default t)
+  
+;; Bootstrap straight
+(defvar bootstrap-version)
+(let ((bootstrap-file
+	   (expand-file-name
+		"straight/repos/straight.el/bootstrap.el"
+		(or (bound-and-true-p straight-base-dir)
+			user-emacs-directory)))
+	  (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+	(with-current-buffer
+		(url-retrieve-synchronously
+		 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+		 'silent 'inhibit-cookies)
+	  (goto-char (point-max))
+	  (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'org)
+
 (defun rebuild-config ()
   (interactive) 
   (org-babel-load-file
