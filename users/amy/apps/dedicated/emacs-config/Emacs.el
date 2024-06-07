c9b024a89a3fa036e702b620dc7511e937be64cb
nullishamy <git@amyerskine.me>
2024-06-07 13:54:22 +0100

    feat: fmt; dtrt indent

--- users/amy/apps/dedicated/emacs-config/Emacs.el
+++ users/amy/apps/dedicated/emacs-config/Emacs.el
@@ -21,94 +21,94 @@
   (setq no-littering-var-directory (expand-file-name "data/" user-emacs-directory)))
 
 (use-package emacs
-  :custom
-  (menu-bar-mode nil)         ;; Disable the menu bar
-  (scroll-bar-mode nil)       ;; Disable the scroll bar
-  (tool-bar-mode nil)         ;; Disable the tool bar
-  ;;(inhibit-startup-screen t)  ;; Disable welcome screen
-  (tooltip-mode -1)           ; Disable tooltips
-  (set-fringe-mode 10)        ; Give some breathing room
-  (toggle-frame-maximized)    ; Always start maximized
+    :custom
+    (menu-bar-mode nil)         ;; Disable the menu bar
+    (scroll-bar-mode nil)       ;; Disable the scroll bar
+    (tool-bar-mode nil)         ;; Disable the tool bar
+    ;;(inhibit-startup-screen t)  ;; Disable welcome screen
+    (tooltip-mode -1)           ; Disable tooltips
+    (set-fringe-mode 10)        ; Give some breathing room
+    (toggle-frame-maximized)    ; Always start maximized
 
-  (delete-selection-mode t)   ;; Select text and delete it by typing.
-  (electric-indent-mode nil)  ;; Turn off the weird indenting that Emacs does by default.
-  (electric-pair-mode t)      ;; Turns on automatic parens pairing
+    (delete-selection-mode t)   ;; Select text and delete it by typing.
+    (electric-indent-mode nil)  ;; Turn off the weird indenting that Emacs does by default.
+    (electric-pair-mode t)      ;; Turns on automatic parens pairing
 
-  (blink-cursor-mode nil)     ;; Don't blink cursor
-  (global-auto-revert-mode t) ;; Automatically reload file and show changes if the file has changed
+    (blink-cursor-mode nil)     ;; Don't blink cursor
+    (global-auto-revert-mode t) ;; Automatically reload file and show changes if the file has changed
 
-  (pixel-scroll-precision-mode 1)
+    (pixel-scroll-precision-mode 1)
 
-  ;;(dired-kill-when-opening-new-dired-buffer t) ;; Dired don't create new buffer
-  ;;(recentf-mode t) ;; Enable recent file mode
+    ;;(dired-kill-when-opening-new-dired-buffer t) ;; Dired don't create new buffer
+    ;;(recentf-mode t) ;; Enable recent file mode
 
-  ;;(global-visual-line-mode t)           ;; Enable truncated lines
-  ;;(display-line-numbers-type 'relative) ;; Relative line numbers
-  (global-display-line-numbers-mode t)  ;; Display line numbers
+    ;;(global-visual-line-mode t)           ;; Enable truncated lines
+    ;;(display-line-numbers-type 'relative) ;; Relative line numbers
+    (global-display-line-numbers-mode t)  ;; Display line numbers
 
-  (mouse-wheel-progressive-speed nil) ;; Disable progressive speed when scrolling
-  (scroll-conservatively 10) ;; Smooth scrolling
-  ;;(scroll-margin 8)
+    (mouse-wheel-progressive-speed nil) ;; Disable progressive speed when scrolling
+    (scroll-conservatively 10) ;; Smooth scrolling
+    ;;(scroll-margin 8)
 
-  (tab-width 4)
+    (tab-width 4)
 
-  (make-backup-files nil) ;; Stop creating ~ backup files
-  (auto-save-default nil) ;; Stop creating # auto save files
+    (make-backup-files nil) ;; Stop creating ~ backup files
+    (auto-save-default nil) ;; Stop creating # auto save files
 
-  :hook
-  (prog-mode . (lambda () (hs-minor-mode t))) ;; Enable folding hide/show globally
-  :config
+    :hook
+    (prog-mode . (lambda () (hs-minor-mode t))) ;; Enable folding hide/show globally
+    :config
 
-  ;; Move customization variables to a separate file and load it, avoid filling up init.el with unnecessary variables
-  (setq custom-file (locate-user-emacs-file "custom-vars.el"))
-  (load custom-file 'noerror 'nomessage)
-  :bind (
-  		 ([escape] . keyboard-escape-quit) ;; Makes Escape quit prompts (Minibuffer Escape)
-  		 )
-  )
+    ;; Move customization variables to a separate file and load it, avoid filling up init.el with unnecessary variables
+    (setq custom-file (locate-user-emacs-file "custom-vars.el"))
+    (load custom-file 'noerror 'nomessage)
+    :bind (
+    	([escape] . keyboard-escape-quit) ;; Makes Escape quit prompts (Minibuffer Escape)
+    		 )
+    )
 
-(use-package diff-hl
-  :hook ((dired-mode         . diff-hl-dired-mode-unless-remote)
-  		 (magit-pre-refresh  . diff-hl-magit-pre-refresh)
-  		 (magit-post-refresh . diff-hl-magit-post-refresh))
-  :init (global-diff-hl-mode))
+  (use-package diff-hl
+    :hook (
+(dired-mode . diff-hl-dired-mode-unless-remote)
+    		 (magit-pre-refresh  . diff-hl-magit-pre-refresh)
+    		 (magit-post-refresh . diff-hl-magit-post-refresh))
+    :init (global-diff-hl-mode))
 
-(use-package diminish)
+  (use-package diminish)
 
-(use-package rainbow-delimiters
-  :hook
-  (prog-mode . rainbow-delimiters-mode)
-  (org-mode . rainbow-delimiters-mode))
+  (use-package rainbow-delimiters
+    :hook
+    (prog-mode . rainbow-delimiters-mode)
+    (org-mode . rainbow-delimiters-mode))
 
-(use-package rainbow-mode
-  :config
-  ;; Make it a proper global mode; we want this everywhere unless we explicitly disable it (TODO: Add blocklist filtering here)
-  (define-globalized-minor-mode global-rainbow-mode rainbow-mode
-  	(lambda () (rainbow-mode 1)))
-  (global-rainbow-mode 1))
+  (use-package rainbow-mode
+    :config
+    ;; Make it a proper global mode; we want this everywhere unless we explicitly disable it (TODO: Add blocklist filtering here)
+    (define-globalized-minor-mode global-rainbow-mode rainbow-mode
+      (lambda () (rainbow-mode 1)))
+    (global-rainbow-mode 1))
 
-(use-package doom-modeline
-  :init (doom-modeline-mode 1)
-  :config
-  (setq doom-modeline-icon nil)
-  (setq doom-modeline-minor-modes t)
-  (setq doom-modeline-lsp t)
-  (setq doom-modeline-buffer-file-name-style 'relative-from-project))
+  (use-package doom-modeline
+    :init (doom-modeline-mode 1)
+    :config
+    (setq doom-modeline-icon nil)
+    (setq doom-modeline-minor-modes t)
+    (setq doom-modeline-lsp t)
+    (setq doom-modeline-buffer-file-name-style 'relative-from-project))
 
-(use-package magit
-  :commands magit-status)
+  (use-package magit
+    :commands magit-status)
 
-(use-package forge
-  :after magit)
+  (use-package forge
+    :after magit)
 
-(use-package git-gutter
-  :config
-  (global-git-gutter-mode 't))
+  (use-package git-gutter
+    :config
+    (global-git-gutter-mode 't))
 
-(use-package zone
-  :disabled
-  :config
-  (zone-when-idle 120))
+  (use-package dtrt-indent
+    :custom
+    (dtrt-indent-global-mode t))
 
 (use-package catppuccin-theme
   :config
@@ -146,9 +146,9 @@
 ;;(add-to-list 'default-frame-alist '(font . "JetBrains Mono")) ;; Set your favorite font
 (setq-default line-spacing 0.12)
 (use-package emacs
-	:bind
-	("<C-wheel-up>" . text-scale-increase)
-	("<C-wheel-down>" . text-scale-decrease))
+  :bind
+  ("<C-wheel-up>" . text-scale-increase)
+  ("<C-wheel-down>" . text-scale-decrease))
 
 (use-package yasnippet
   :config
@@ -217,13 +217,13 @@
   (which-key-allow-imprecise-window-fit nil)) ;; Fixes which-key window slipping out in Emacs Daemon
 
 (use-package direnv
- :config
- (direnv-mode)
- (setq direnv-always-show-summary nil))
+  :config
+  (direnv-mode)
+  (setq direnv-always-show-summary nil))
 
 (use-package expand-region
- :config
- (global-set-key (kbd "C-x e") 'er/expand-region))
+  :config
+  (global-set-key (kbd "C-x e") 'er/expand-region))
 
 (defun indent-region-advice (&rest ignored)
   (let ((deactivate deactivate-mark))
@@ -248,17 +248,17 @@
  (define-key projectile-mode-map (kbd "M-p") 'projectile-command-map))
 
 (use-package multiple-cursors
-:config
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this))
+  :config
+  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this))
 
 (use-package beacon
   :config
   (beacon-mode 1))
 
 (use-package goto-line-preview
-:config
-(global-set-key [remap goto-line] 'goto-line-preview))
+  :config
+  (global-set-key [remap goto-line] 'goto-line-preview))
 
 (use-package anzu
 :config
@@ -267,50 +267,50 @@
 (global-set-key [remap query-replace-regexp] 'anzu-query-replace-regexp))
 
 (use-package lsp-mode
-	:config
-	(setq lsp-keymap-prefix "C-c l")
-	(setq lsp-completion-enable nil)
-	(setq lsp-completion-provider :capf)
-	(setq lsp-modeline-code-actions-enable nil)
-	(setq lsp-modeline-diagnostics-enable nil)
-	(setq lsp-modeline-workspace-status-enable nil)
-
-	:hook (
-		   (python-mode . lsp)
-		   (rust-mode . lsp)
-		   (svelte-mode . lsp)
-		   (go-mode . lsp)
-		   (nix-mode . lsp)
-		   (typescript-mode . lsp)
-		   (zig-mode . lsp)
-		   (lsp-mode . lsp-enable-which-key-integration))
-	:commands lsp)
+  :config
+  (setq lsp-keymap-prefix "C-c l")
+  (setq lsp-completion-enable nil)
+  (setq lsp-completion-provider :capf)
+  (setq lsp-modeline-code-actions-enable nil)
+  (setq lsp-modeline-diagnostics-enable nil)
+  (setq lsp-modeline-workspace-status-enable nil)
+  
+  :hook (
+		 (python-mode . lsp)
+		 (rust-mode . lsp)
+		 (svelte-mode . lsp)
+		 (go-mode . lsp)
+		 (nix-mode . lsp)
+		 (typescript-mode . lsp)
+		 (zig-mode . lsp)
+		 (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
 
 (use-package lsp-ui
-	:commands
-	lsp-ui-mode
-	:config
-	(setq lsp-ui-doc-position 'at-point)
-	(setq lsp-ui-doc-delay 1.5)
-	(keymap-global-set "C-c d s" 'lsp-ui-doc-show)
-	(keymap-global-set "C-c d f" 'lsp-ui-doc-focus-frame)
-	(keymap-global-set "C-c d h" 'lsp-ui-doc-hide)
-	(setq lsp-ui-doc-enable t))
+  :commands
+  lsp-ui-mode
+  :config
+  (setq lsp-ui-doc-position 'at-point)
+  (setq lsp-ui-doc-delay 1.5)
+  (keymap-global-set "C-c d s" 'lsp-ui-doc-show)
+  (keymap-global-set "C-c d f" 'lsp-ui-doc-focus-frame)
+  (keymap-global-set "C-c d h" 'lsp-ui-doc-hide)
+  (setq lsp-ui-doc-enable t))
 
 ;; Filetype -> mode mappings
 (setq auto-mode-alist
-		(append
+	  (append
 		 ;; File name (within directory) starts with a dot.
-		 '(("/\\.[^/]*\\'" . fundamental-mode)
-		   ;; File name has no dot.
-		   ("/[^\\./]*\\'" . fundamental-mode)
-		   ;; File name ends in ‘.el’.
-		   ("\\.el\\'" . emacs-lisp-mode)
-		   ("\\.zig\\'" . zig-mode)
-		   ;; Git modes
-		   (".*git-rebase-todo" . git-rebase-mode)
-		   (".*COMMIT_EDITMSG" . git-rebase-mode))
-		 auto-mode-alist))
+	   '(("/\\.[^/]*\\'" . fundamental-mode)
+		 ;; File name has no dot.
+		 ("/[^\\./]*\\'" . fundamental-mode)
+		 ;; File name ends in ‘.el’.
+		 ("\\.el\\'" . emacs-lisp-mode)
+		 ("\\.zig\\'" . zig-mode)
+		 ;; Git modes
+		 (".*git-rebase-todo" . git-rebase-mode)
+		 (".*COMMIT_EDITMSG" . git-rebase-mode))
+	   auto-mode-alist))
 
 ;; Additional language modes
 (use-package nix-mode
@@ -341,20 +341,20 @@
 
 ;; In-buffer checking
 (use-package flycheck
-	:config
-	(add-hook 'after-init-hook #'global-flycheck-mode))
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode))
 
 (use-package flycheck-inline
-	:config
-	(with-eval-after-load 'flycheck
-	  (add-hook 'flycheck-mode-hook #'flycheck-inline-mode)))
+  :config
+  (with-eval-after-load 'flycheck
+	(add-hook 'flycheck-mode-hook #'flycheck-inline-mode)))
 
 (use-package toc-org
-	:commands toc-org-enable
-	:hook (org-mode . toc-org-mode))
+  :commands toc-org-enable
+  :hook (org-mode . toc-org-mode))
 
 (use-package org-superstar
-	:hook (org-mode . org-superstar-mode))
+  :hook (org-mode . org-superstar-mode))
 
 ;; https://www.masteringemacs.org/article/introduction-to-ido-mode
 (setq ido-enable-flex-matching t)
@@ -362,61 +362,61 @@
 (ido-mode 1)
 
 (use-package ido-completing-read+
-	:config
-	(ido-ubiquitous-mode 1))
+  :config
+  (ido-ubiquitous-mode 1))
 
 
 (savehist-mode) ;; Enables save history mode
 
 (use-package corfu
-	:custom
-	(corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-	(corfu-auto t)                 ;; Enable auto completion
-	(corfu-auto-prefix 2)          ;; Minimum length of prefix for auto completion.
-	(corfu-popupinfo-mode t)       ;; Enable popup information
-	(corfu-popupinfo-delay 0.5)    ;; Lower popupinfo delay to 0.5 seconds from 2 seconds
-	(corfu-separator ?\s)          ;; Orderless field separator, Use M-SPC to enter separator
-	;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-	;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-	(corfu-preview-current t)    ;; Disable current candidate preview
-	;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-	;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-	;; (corfu-scroll-margin 5)        ;; Use scroll margin
-	(completion-ignore-case t)
-	;; Enable indentation+completion using the TAB key.
-	;; `completion-at-point' is often bound to M-TAB.
-	(tab-always-indent 'complete)
-	(corfu-preview-current nil) ;; Don't insert completion without confirmation
-	;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
-	;; be used globally (M-/).  See also the customization variable
-	;; `global-corfu-modes' to exclude certain modes.
-	:init
-	(global-corfu-mode))
+  :custom
+  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  (corfu-auto t)                 ;; Enable auto completion
+  (corfu-auto-prefix 2)          ;; Minimum length of prefix for auto completion.
+  (corfu-popupinfo-mode t)       ;; Enable popup information
+  (corfu-popupinfo-delay 0.5)    ;; Lower popupinfo delay to 0.5 seconds from 2 seconds
+  (corfu-separator ?\s)          ;; Orderless field separator, Use M-SPC to enter separator
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  (corfu-preview-current t)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+  (completion-ignore-case t)
+  ;; Enable indentation+completion using the TAB key.
+  ;; `completion-at-point' is often bound to M-TAB.
+  (tab-always-indent 'complete)
+  (corfu-preview-current nil) ;; Don't insert completion without confirmation
+  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
+  ;; be used globally (M-/).  See also the customization variable
+  ;; `global-corfu-modes' to exclude certain modes.
+  :init
+  (global-corfu-mode))
 
 (use-package cape
-	:after corfu
-	:config
-	;; Add to the global default value of `completion-at-point-functions' which is
-	;; used by `completion-at-point'.  The order of the functions matters, the
-	;; first function returning a result wins.  Note that the list of buffer-local
-	;; completion functions takes precedence over the global list.
-	;; The functions that are added later will be the first in the list
+  :after corfu
+  :config
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  ;; The functions that are added later will be the first in the list
 	
-	(add-to-list 'completion-at-point-functions #'cape-dabbrev) ;; Complete word from current buffers
-	(add-to-list 'completion-at-point-functions #'cape-dict) ;; Dictionary completion
-	(add-to-list 'completion-at-point-functions #'cape-file) ;; Path completion
-	(add-to-list 'completion-at-point-functions #'cape-elisp-block) ;; Complete elisp in Org or Markdown mode
-	(add-to-list 'completion-at-point-functions #'cape-keyword) ;; Keyword/Snipet completion
-
-	(keymap-global-set "M-TAB" 'completion-at-point)
-	;;(add-to-list 'completion-at-point-functions #'cape-abbrev) ;; Complete abbreviation
-	;;(add-to-list 'completion-at-point-functions #'cape-history) ;; Complete from Eshell, Comint or minibuffer history
-	;;(add-to-list 'completion-at-point-functions #'cape-line) ;; Complete entire line from current buffer
-	;;(add-to-list 'completion-at-point-functions #'cape-elisp-symbol) ;; Complete Elisp symbol
-	;;(add-to-list 'completion-at-point-functions #'cape-tex) ;; Complete Unicode char from TeX command, e.g. \hbar
-	;;(add-to-list 'completion-at-point-functions #'cape-sgml) ;; Complete Unicode char from SGML entity, e.g., &alpha
-	;;(add-to-list 'completion-at-point-functions #'cape-rfc1345) ;; Complete Unicode char using RFC 1345 mnemonics
-	)
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev) ;; Complete word from current buffers
+  (add-to-list 'completion-at-point-functions #'cape-dict) ;; Dictionary completion
+  (add-to-list 'completion-at-point-functions #'cape-file) ;; Path completion
+  (add-to-list 'completion-at-point-functions #'cape-elisp-block) ;; Complete elisp in Org or Markdown mode
+  (add-to-list 'completion-at-point-functions #'cape-keyword) ;; Keyword/Snipet completion
+  
+  (keymap-global-set "M-TAB" 'completion-at-point)
+  ;;(add-to-list 'completion-at-point-functions #'cape-abbrev) ;; Complete abbreviation
+  ;;(add-to-list 'completion-at-point-functions #'cape-history) ;; Complete from Eshell, Comint or minibuffer history
+  ;;(add-to-list 'completion-at-point-functions #'cape-line) ;; Complete entire line from current buffer
+  ;;(add-to-list 'completion-at-point-functions #'cape-elisp-symbol) ;; Complete Elisp symbol
+  ;;(add-to-list 'completion-at-point-functions #'cape-tex) ;; Complete Unicode char from TeX command, e.g. \hbar
+  ;;(add-to-list 'completion-at-point-functions #'cape-sgml) ;; Complete Unicode char from SGML entity, e.g., &alpha
+  ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345) ;; Complete Unicode char using RFC 1345 mnemonics
+  )
 
 (use-package yasnippet-capf
   :after cape
@@ -425,10 +425,10 @@
   (add-to-list 'completion-at-point-functions #'yasnippet-capf))
 
 (defun elcord--enable-on-frame-created (f)
-	(elcord-mode +1))
+  (elcord-mode +1))
 
 (defun elcord--disable-elcord-if-no-frames (f)
-	(when (let ((frames (delete f (visible-frame-list))))
+  (when (let ((frames (delete f (visible-frame-list))))
 		  (or (null frames)
 			  (and (null (cdr frames))
 				   (eq (car frames) terminal-frame))))
@@ -441,10 +441,10 @@
 	  (remove-hook 'delete-frame-functions 'elcord--disable-elcord-if-no-frames)))
 
 (use-package elcord
-	:config
-	(add-hook 'elcord-mode-hook 'custom-elcord-mode-hook)
-	(elcord-mode)
-	(setq elcord-quiet t)
+  :config
+  (add-hook 'elcord-mode-hook 'custom-elcord-mode-hook)
+  (elcord-mode)
+  (setq elcord-quiet t)
 	(setq elcord-idle-message "AFK.."))
 
 (use-package yagist
@@ -452,21 +452,21 @@
   (setq yagist-view-gist t))
 
 (use-package nerd-icons-completion
-	:config
-	(nerd-icons-completion-mode))
+  :config
+  (nerd-icons-completion-mode))
 
 (use-package nerd-icons-corfu
-	:after corfu
-	:init (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
+  :after corfu
+  :init (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
 
 (use-package nerd-icons
-	:if (display-graphic-p))
+  :if (display-graphic-p))
 
 (use-package all-the-icons
   :if (display-graphic-p))
 
 (use-package nerd-icons-dired
-	:hook (dired-mode . (lambda () (nerd-icons-dired-mode t))))
+  :hook (dired-mode . (lambda () (nerd-icons-dired-mode t))))
 
 (use-package nerd-icons-ibuffer
-	:hook (ibuffer-mode . nerd-icons-ibuffer-mode))
+  :hook (ibuffer-mode . nerd-icons-ibuffer-mode))
