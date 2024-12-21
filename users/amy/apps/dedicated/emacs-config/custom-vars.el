38dd523b09236012686c9909b2cfcf173551a57b
nullishamy <git@amyerskine.me>
2024-12-21 13:42:25 +0000

    feat: ivy; aesthetics

--- users/amy/apps/dedicated/emacs-config/custom-vars.el
+++ users/amy/apps/dedicated/emacs-config/custom-vars.el
@@ -3,10 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("77fff78cc13a2ff41ad0a8ba2f09e8efd3c7e16be20725606c095f9a19c24d3d" "0c83e0b50946e39e237769ad368a08f2cd1c854ccbcd1a01d39fdce4d6f86478" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "dccf4a8f1aaf5f24d2ab63af1aa75fd9d535c83377f8e26380162e888be0c6a9" default))
+ '(jumplist-ex-mode t)
+ '(jumplist-hook-commands
+   '(dired-jump isearch-forward end-of-buffer beginning-of-buffer find-file))
  '(package-selected-packages
    '(gnu-elpa-keyring-update which-key rainbow-mode rainbow-delimiters diminish nerd-icons-completion cape nerd-icons-corfu corfu ido-completing-read+ diff-hl magit nerd-icons-ibuffer nerd-icons-dired nerd-icons eat org-superstar toc-org yasnippet-snippets hl-todo lsp-ui elcord lsp-tailwindcss svelte-mode rust-mode go-mode nix-mode lsp-mode projectile catppuccin-theme direnv flycheck-inline markdown-mode typescript-mode use-package))
  '(wakatime-cli-path "~/.wakatime/wakatime-cli"))
-(custom-set-faces 
+(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
